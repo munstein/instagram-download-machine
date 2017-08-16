6d84ebc3c65fc5cca3f4a8298a37e23832be4c48
@munstein <eduardo.munstein@live.com>
2017-08-16 18:21:35 -0300

    Add Demo

---
+++ Demo Project/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InstagramDownloadMachineLib;
+
+namespace InstagramDownloadMachineDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Download started");
+            Download();
+            Console.WriteLine("Downloading...");            
+        }
+
+        public static async void Download()
+        {
+            InstagramDownloadMachine instagramDownloadMachine = new InstagramDownloadMachine();
+            await instagramDownloadMachine.DownloadToPath("https://www.instagram.com/p/BXEJU-jDs94", "img.jpg");
+            Console.WriteLine("Download ready!");
+            Console.ReadLine();
+        }
+    }
+}
